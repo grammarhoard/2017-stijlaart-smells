19dbc42966594213b58b8dca96dce224d947e58f
Mats Stijlaart <m.stijlaart@avisi.nl>
2017-04-28 13:08:24 +0200

    Add setup_grammarlab script

--- grammarsmells/add_zoo.sh
+++ grammarsmells/add_zoo.sh
@@ -3,6 +3,8 @@
 rm -rf input/zoo
 rm -rf zoo-clone
 git clone https://github.com/slebok/zoo.git zoo-clone
+(cd zoo-clone && git checkout 51ba8ff5a561337b037c58fe9bf833fff8aaea57)
+
 cp -rf zoo-clone/zoo input/zoo
 
 rm -rf zoo-clone
